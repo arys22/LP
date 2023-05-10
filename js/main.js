d579c9fdbc6995afae16a70428edd80f64e90b75
arys-s <87214988+arys-s@users.noreply.github.com>
2023-05-11 08:39:36 +0900

    css TOPBTN 修正

--- js/main.js
+++ js/main.js
@@ -35,33 +35,6 @@ window.addEventListener("scroll", function () {
   line3.classList.toggle("change", nowY > fv - 30);
 });
 
-// topへ戻るbtn
-function scrollTop(el, duration) {
-  const target = document.getElementById(el);
-  target.addEventListener("click", function () {
-    let currentY = window.pageYOffset; // 現在のスクロール位置を取得
-    let step = duration / currentY > 1 ? 10 : 100; // 三項演算子
-    let timeStep = (duration / currentY) * step; // スクロール時間
-    let intervalId = setInterval(scrollUp, timeStep);
-    // timeStepの間隔でscrollUpを繰り返す。再帰的
-    // clearItervalのために返り値intervalIdを定義する。
-
-    // setInterval()繰り返し処理
-    // clearIterval()⇧ストップ
-
-    function scrollUp() {
-      currentY = window.pageYOffset;
-      if (currentY === 0) {
-        clearInterval(intervalId); // ページ最上部に来たら終了
-      } else {
-        scrollBy(0, -step); // step分上へスクロール
-      }
-    }
-  });
-}
-// スクロールトップボタン
-scrollTop("page-top", 180); // 遅すぎるとガクガクになるので注意
-
 // スクロール表示
 const pagetop = document.getElementById("page-top");
 window.addEventListener("scroll", function () {
